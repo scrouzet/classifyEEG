8babff75b6c28cfb7efa00cd100cf5d274745e73
Sébastien Crouzet <sebastien@MBPSeb.local>
2014-10-08 10:06:04 +0200

    Add the option to not plot average
    This is useful for plotting chance

--- sliding_plot.m
+++ sliding_plot.m
@@ -2,18 +2,17 @@ function[] = sliding_plot(mytimes, dec_acc, mycolor, dec_acc_ci)
 % plot the classification performance over time: 
 % - fill for the CI
 % - line for the average across cv
-%
-% TODO
-% add options for plotting chance level (fill or dotted line)
 
 if nargin > 3
     % Plot CI
-    fill([mytimes fliplr(mytimes) ], [ dec_acc_ci(1,:) fliplr(dec_acc_ci(2,:)) ], mycolor, 'EdgeColor', 'none', 'FaceAlpha', 0.5); hold on
+    fill([mytimes fliplr(mytimes) ], [ dec_acc_ci(1,:) fliplr(dec_acc_ci(2,:)) ],...
+        mycolor, 'EdgeColor', 'none', 'FaceAlpha', 0.3); hold on
 end
 
-% Plot average across CV
-plot(mytimes, dec_acc, 'Color', mycolor); hold on
-
+if ~isempty(dec_acc)
+    % Plot average across CV
+    plot(mytimes, dec_acc, 'Color', mycolor); hold on
+end
 
 %    p_masked = double(sigtestClassifEEG(res(z).classif.accuracy(:,:,1), res(z).classif.accuracy_rand(:,:,1), alpha, nsucc, 'ttest_randlabel'));
 %    plot(times(p_masked),repmat(ysig(1),length(times(p_masked)),1),'s','MarkerEdgeColor','none','MarkerFaceColor',mycolors(1,:), 'MarkerSize',4); hold on
